1fdb7551b479ede8cafdbd4cb21ebc420fd0bf2b
David Marsh <40401344+Daveym1@users.noreply.github.com>
2022-12-20 16:40:08 +0000

    Added ajax request function and linked it to button event

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,56 @@ var searchInput = document.getElementById("search-input");
 var searchHistory = document.getElementById("history");
 var historyButton = $("<button>");
 var historyList = $("#history-list");
+var apiKey = "42eadeef9d5a4dba9f2ba031ca03c827";
+var cityName = "London";
+var queryURL =
+  "https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&cnt=3&appid=" +
+  apiKey;
 
-var cities = [];
+// function showWeather() {
+//   let xhr = new XMLHttpRequest();
 
-searchButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log(searchInput.value);
-  historyList.append(
-    "<li>" + "<button>" + searchInput.value + "</button>" + "</li>"
-  );
-  $("li").addClass("cities");
-  $("button").addClass("citiesButton");
-});
+//   xhr.open("GET", queryURL, true);
+
+//   xhr.onload = function () {
+//     if (xhr.status == 200) {
+//       console.log("success");
+//       let result = this.response;
+//       console.log(result);
+//     }
+//   };
+//   xhr.send();
+// }
+
+// showWeather();
+
+
+
+$(document).ready(function () {
+  
+ // Search button, when clicked adds the city name to the history list
+
+  searchButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(searchInput.value);
+    cityName = searchInput.value;
+    historyList.append(
+      "<li>" + "<button>" + searchInput.value + "</button>" + "</li>"
+    );
+    $("li").addClass("cities");
+    $("button").addClass("citiesButton");
+
+    function test() {
+
+      $.ajax({
+          method: 'GET',
+          url: 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + apiKey
+      }).then(function (response) {
+          console.log(response);
+      });
+  }
+  test();
+  });
+  });
+
+
