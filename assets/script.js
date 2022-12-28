c6e3984e50195814886edeb2d6abd66ba97e47df
David Marsh <40401344+Daveym1@users.noreply.github.com>
2022-12-28 11:08:28 +0000

    got buttons working

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,18 @@ var queryURL =
   "https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&cnt=3&appid=" +
   apiKey;
 var coords;
+var todayWeather = $("#today");
+var getWeather;
+var city = $("li.cities");
+var buttonValue;
+var citiesButton = $(".citiesButton");
+var cities = $(".cities");
+var today = $("#today");
+var forecast = $("#forecast");
 
 // document ready function
 
 $(document).ready(function () {
-
   // Search button, when clicked adds the city name to the history list
 
   searchButton.addEventListener("click", function (event) {
@@ -23,10 +30,12 @@ $(document).ready(function () {
     historyList.append(
       "<li>" + "<button>" + searchInput.value + "</button>" + "</li>"
     );
-    $("li").addClass("cities");
-    $("button").addClass("citiesButton");
 
-    //calling the OpenWeather API 
+    $("li").addClass("cities");
+
+    $("li.cities > button").addClass("citiesButton");
+
+    //calling the OpenWeather API
 
     $.getJSON(
       "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -34,18 +43,56 @@ $(document).ready(function () {
         "&limit=1&appid=" +
         apiKey,
       function (data) {
-
-      // function to get the latitude and logitude from the city name
+        // function to get the latitude and logitude from the city name
 
         function latLon() {
           coords = [data[0].lat, data[0].lon];
           console.log(coords);
-          return;
         }
         latLon();
-      }
-    );
+
+        //API call to get the weather for co-ordinates which we retrieved in the first API call
+
+        getWeather = function () {
+          $.getJSON(
+            "http://api.openweathermap.org/data/2.5/forecast?lat=" +
+              coords[0] +
+              "&lon=" +
+              coords[1] +
+              "&cnt=1&appid=" +
+              apiKey,
+            function (response) {
+              console.log(response);
+              todayWeather.text("");
+              todayWeather.append(
+                "<h3>" +
+                  "Today's Weather in " +
+                  response.city.name +
+                  ": " +
+                  "</h3>"
+
+                  +
+
+                  "<p>" + "Today's Weather" + "</p>"
+                  
+                  
+                  );
+                  forecast.append(
+                    "<p>" + "5 day forcast" + "</p>"
+                  )
+                  
+            }
+            );
+          };
+          getWeather();
+        }
+        );
+              forcast.text("It worked");
   });
 
-  
+  //Generated history list buttons when click display the weather for that city
+
+  $(document).on("click", ".citiesButton", function () {
+    console.log($(this).text());
+  });
 });
